49159163a09fd268ff47380cc7d9a0f6887f4b3d
Chris Debenham <chris@adebenham.com>
2019-05-06 20:12:39 +1000

    Multiple updates

--- ptz_script.sh
+++ ptz_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl "http://192.168.0.232/media/?action=cmd&code=2&value=${1}&user=admin&pwd="
+curl "http://192.168.86.41/media/?action=cmd&code=2&value=${1}&user=admin&pwd="
 sleep .2
-curl "http://192.168.0.232/media/?action=cmd&code=3&value=${1}&user=admin&pwd="
+curl "http://192.168.86.41/media/?action=cmd&code=3&value=${1}&user=admin&pwd="
 date >> /tmp/a
 
