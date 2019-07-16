9e327e413a8fcf9cb36c22f4b1ff0924927a014d
Chris Debenham <chris@adebenham.com>
2019-07-16 13:28:18 +1000

    Stop using onvif for camera and remove old camera

--- ptz_script.sh
+++ ptz_script.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
-curl "http://192.168.86.41/media/?action=cmd&code=2&value=${1}&user=admin&pwd="
-sleep .2
-curl "http://192.168.86.41/media/?action=cmd&code=3&value=${1}&user=admin&pwd="
-date >> /tmp/a
-
+BASE="http://192.168.86.74/decoder_control.cgi?loginuse=admin&loginpas=admin&onestep=0&15632464760490.1568509176296169&_=1563246476049&command="
+DELAY=.3
+if [ "$1" = "right" ]
+  then curl "${BASE}6"
+  sleep $DELAY
+  curl "${BASE}7"
+elif [ "$1" = "left" ]
+  then curl "${BASE}4"
+  sleep $DELAY
+  curl "${BASE}5"
+elif [ "$1" = "down" ]
+  then curl "${BASE}2"
+  sleep $DELAY
+  curl "${BASE}3"
+elif [ "$1" = "up" ]
+  then curl "${BASE}0"
+  sleep $DELAY
+  curl "${BASE}1"
+fi
