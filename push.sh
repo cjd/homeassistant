a0711c75125edbbebfa50b81519993e8123a6d0e
Chris Debenham <chris@adebenham.com>
2019-09-09 09:36:49 +1000

    Remove old script

--- push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/home/cjd/Nextcloud/Apps/bin/bw.bin config server https://home.adebenham.com/
-export BW_SESSION=`bw.bin login chris@adebenham.com --raw`
-git push https://cjd:`bw.bin get password "github cli"`@github.com/cjd/homeassistant.git
-bw.bin logout
