41540a7db48ce71f503597773c52e426319d0bd7
Chris Debenham <chris@adebenham.com>
2019-11-05 11:20:09 +1100

    Script update

--- clean_db.sh
+++ clean_db.sh
@@ -1,1 +1,3 @@
+ENTITIES="sensor.tv_power_current 
+CMD="docker-exec mysql mysql -uhass -pdhtfgujgftyfgj -h192.168.86.2 homeassistant"
 for F in `grep " *-.*\." recorder.yaml |sed -e 's/^.*- //g'`;do sqlite3 home-assistant_v2.db "delete from states where entity_id='$F'";done
