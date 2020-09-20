61fa40cf4d35a8657202e9be955aa3a8cd2bb26e
Chris Debenham <chris@adebenham.com>
2020-09-21 08:36:10 +1000

    HACS

--- custom_components/entity_controller/const.py
+++ custom_components/entity_controller/const.py
@@ -21,8 +21,8 @@ DOMAIN = "entity_controller"
 
 # services
 SERVICE_CLEAR_BLOCK = "clear_block"
-SERVICE_SET_STAY_ON = "set_stay_on"
-SERVICE_SET_STAY_OFF = "set_stay_off"
+SERVICE_ENABLE_STAY_MODE = "enable_stay_mode"
+SERVICE_DISABLE_STAY_MODE = "disable_stay_mode"
 SERVICE_SET_NIGHT_MODE = "set_night_mode"
 
 #configuration
@@ -79,3 +79,4 @@ CONSTRAIN_END = 2
 STATES = ['idle', 'overridden', 'constrained', 'blocked',
           {'name': 'active', 'children': ['timer', 'stay_on'],
            'initial': False}]
+CONF_IGNORE_STATE_CHANGES_UNTIL = "grace_period"
